2c9211e11e5c13e1143d21bef687e1c81645d0d7
Hardian Prakasa <hardian.prakasa@mail.ugm.ac.id>
2015-05-12 13:30:25 +0700

    Pembuatan interface kasar dan fungsi perhitungan diskon dasar

--- app/src/main/java/com/example/streethardy/discountcalc/MainActivity.java
+++ app/src/main/java/com/example/streethardy/discountcalc/MainActivity.java
@@ -4,14 +4,28 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button buttonHitung;
+    EditText editTextHarga,editTextDiskon, editTextHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextHarga= (EditText) findViewById(R.id.boxHarga);
+        editTextDiskon= (EditText) findViewById(R.id.boxDiskon);
+        editTextHasil= (EditText) findViewById(R.id.textResult);
+
+        buttonHitung= (Button) findViewById(R.id.btnHitung);
+
+        buttonHitung.setOnClickListener(this);
     }
 
 
@@ -35,5 +49,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String harga=editTextHarga.getText().toString();
+        String diskon=editTextDiskon.getText().toString();
+
+        double hasil = Double.parseDouble(harga)-(Double.parseDouble(harga)*Double.parseDouble(diskon)/100);
+
+        editTextHasil.setText(String.valueOf(hasil));
     }
 }
