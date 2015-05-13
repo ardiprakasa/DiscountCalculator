2f3cf96a8ca0b0dba5e8b57ce3a719af1d5017d9
Arwan <arwan4194@gmail.com>
2015-05-13 19:00:22 +0700

    what's new : - validasi input - penataan background dan layout

--- app/src/main/java/com/example/streethardy/discountcalc/MainActivity.java
+++ app/src/main/java/com/example/streethardy/discountcalc/MainActivity.java
@@ -2,25 +2,79 @@ package com.example.streethardy.discountcalc;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     Button buttonHitung;
     EditText editTextHarga,editTextDiskon, editTextHasil;
+    TextView warningText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        warningText= (TextView) findViewById(R.id.WarningText);
+        warningText.setVisibility(TextView.INVISIBLE);
         editTextHarga= (EditText) findViewById(R.id.boxHarga);
+        editTextHarga.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (editTextDiskon.getText().toString().isEmpty())
+                {
+
+                }
+               else if (Long.parseLong(editTextHarga.getText().toString()) < 0) {
+                    editTextHarga.setText("harga tidak boleh negatif");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         editTextDiskon= (EditText) findViewById(R.id.boxDiskon);
+       editTextDiskon.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence s, int start, int before, int count) {
+               if (editTextDiskon.getText().toString().isEmpty())
+               {
+
+               }
+                else if (Long.parseLong(editTextDiskon.getText().toString()) < 45 || Long.parseLong(editTextDiskon.getText().toString()) > 75)
+                {
+                   warningText.setVisibility(TextView.VISIBLE);
+                }
+               else
+                {
+                    warningText.setVisibility(warningText.INVISIBLE);
+                }
+           }
+
+           @Override
+           public void afterTextChanged(Editable s) {
+
+           }
+       });
         editTextHasil= (EditText) findViewById(R.id.textResult);
 
         buttonHitung= (Button) findViewById(R.id.btnHitung);
@@ -44,9 +98,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        if (id == R.id.action_settings) return true;
 
         return super.onOptionsItemSelected(item);
     }
